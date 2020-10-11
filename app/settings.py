7c62ef8b6e22515758c7b737b0c625b06245d264
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2020-10-11 16:57:16 +0100

    New hidden secret key

--- app/settings.py
+++ app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7fv#djdu#_35pv5-a1390i!@v8b7e&b43=_01+1u!36+c+j2(x'
+SECRET_KEY = os.environ.get('DJANGOCF_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
